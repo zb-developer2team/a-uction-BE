233e99c404b3e3896089eb60d5f49703d658bb76
JinSol <wlsthf0003@naver.com>
2023-04-05 16:26:56 +0900

    [Feat/#16/kakaoToken] 카카오 토큰 가져오기 카카오 인가코드를 사용하여 AccessToken 가져오기

---
+++ a_uction/src/main/java/com/example/a_uction/user/controller/KakaoController.java
@@ -0,0 +1,27 @@
+package com.example.a_uction.user.controller;
+
+import com.example.a_uction.user.oauth.service.KakaoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/oauth")
+@RequiredArgsConstructor
+public class KakaoController {
+
+	private final KakaoService kakaoService;
+
+	@RequestMapping("/kakao/callback")
+	public ResponseEntity<?> kakaoCallback(@RequestParam(name = "code") String code) {
+		log.info("[CODE = {}]", code);
+
+		String accessToken = kakaoService.getAccessToken(code);
+		return ResponseEntity.ok(accessToken);
+		// 현재 테스트 위해서 토큰 return , 추후에 리턴 값은 변경 할 것임
+	}
+}
---
+++ a_uction/src/main/java/com/example/a_uction/user/oauth/service/KakaoService.java
@@ -0,0 +1,100 @@
+package com.example.a_uction.user.oauth.service;
+
+import java.util.HashMap;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class KakaoService {
+
+	@Value("${oauth2.kakao.restApiKey}")
+	private String REST_API_KEY;
+	@Value("${oauth2.kakao.redirectUrl}")
+	private String REDIRECT_URL;
+
+	public String getAccessToken (String authorize_code) {
+
+		String reqURL = "https://kauth.kakao.com/oauth/token";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", REST_API_KEY );
+		params.add("redirect_url", REDIRECT_URL);
+		params.add("code", authorize_code);
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpEntity<MultiValueMap<String,String>> kakaoTokenReq= new HttpEntity<>(params, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+			reqURL,
+			HttpMethod.POST,
+			kakaoTokenReq,
+			String.class
+		);
+
+		String tokenJson = response.getBody();
+
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject;
+
+		try {
+			jsonObject = (JSONObject) jsonParser.parse(tokenJson);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+
+		String accessToken = jsonObject.get("access_token").toString();
+		return accessToken;
+	}
+
+	public HashMap<String, String> getUserInfoByKakao (String accessToken) {
+		String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + accessToken);
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpEntity<MultiValueMap<String,String>> kakaoUserReq= new HttpEntity<>(headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+			reqURL,
+			HttpMethod.POST,
+			kakaoUserReq,
+			String.class
+		);
+
+		String userInfoJson = response.getBody();
+
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject;
+
+		try {
+			jsonObject = (JSONObject) jsonParser.parse(userInfoJson);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+
+		HashMap<String, String> userInfo = new HashMap<>();
+		JSONObject properties = (JSONObject) jsonObject.get("properties");
+		JSONObject kakao_account = (JSONObject) jsonObject.get("kakao_account");
+
+		userInfo.put("nickname",properties.get("nickname").toString());
+		userInfo.put("email", kakao_account.get("email").toString());
+
+		return userInfo;
+	}
+}
