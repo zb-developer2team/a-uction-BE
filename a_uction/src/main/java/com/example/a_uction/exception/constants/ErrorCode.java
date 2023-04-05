5c0ac1dc5ebd3afd8295a78b51fc7df5fb61b9f0
sunsik <sunsig21c@gmail.com>
2023-04-05 22:20:00 +0900

    feat : [feat/#15/CustomException/0] CustomException 생성
    naming - AuctionException
    클라이언트 오류 발생시 하나씩 추가하셔야합니다~!

---
+++ a_uction/src/main/java/com/example/a_uction/exception/constants/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.example.a_uction.exception.constants;
+
+import static org.springframework.http.HttpStatus.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"내부 서버 오류가 발생하였습니다."),
+	DATABASE_CONSTRAINTS_VIOLATED(BAD_REQUEST, "중복된 데이터가 있거나 데이터 값이 다릅니다."),
+	INVALID_REQUEST(BAD_REQUEST, "입력하신 데이터를 확인해 주세요. 잘못 된 요청입니다."),
+
+
+	EMAIL_FORMAT_ERROR(BAD_REQUEST, "이메일 형식이 올바르지 않습니다."),
+	THIS_EMAIL_ALREADY_EXIST(BAD_REQUEST, "해당 이메일은 이미 존재합니다.");
+	private final HttpStatus httpStatus;
+	private final String description;
+}
--- a_uction/src/main/java/com/example/a_uction/model/user/entity/UserEntity.java
+++ a_uction/src/main/java/com/example/a_uction/model/user/entity/UserEntity.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,6 +24,8 @@ public class UserEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Email
+	@NotNull
 	private String userEmail;
 	private String password;
 	private String username;
