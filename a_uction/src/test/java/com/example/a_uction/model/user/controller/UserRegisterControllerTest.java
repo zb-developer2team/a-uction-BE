7efb108583e175e810895253710ca1d8b46788bd
sunsik <sunsig21c@gmail.com>
2023-04-05 16:46:05 +0900

    feat : [feat/#13/register/2] 회원가입 테스트
    회원가입성공 컨트롤러 테스트 구현
    회원가입성공 서비스 테스트 구현
    CustomException 먼저 생성 후에 CustomException 발생하는 테스트를 구현 할 예정

---
+++ a_uction/src/test/java/com/example/a_uction/model/user/controller/UserRegisterControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.a_uction.model.user.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.a_uction.model.user.dto.RegisterUser;
+import com.example.a_uction.model.user.repository.UserRepository;
+import com.example.a_uction.model.user.service.UserRegisterService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class UserRegisterControllerTest {
+
+	@MockBean
+	private UserRegisterService userRegisterService;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private MockMvc mockMvc;
+	@Test
+	@WithMockUser
+	void register_SUCCESS() throws Exception {
+	    //given
+		given(userRegisterService.register(any()))
+			.willReturn(RegisterUser.builder()
+				.userEmail("zerobase@gmail.com")
+				.username("zerobase")
+				.build());
+	    //when
+	    //then
+		mockMvc.perform(post("/register")
+				.with(csrf())
+				.content(objectMapper.writeValueAsString(
+					new RegisterUser.Request(
+						"zerobase@gmail.com",
+						"1234",
+						"zerobase",
+						"01012345678")
+				))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.userEmail").value("zerobase@gmail.com"))
+			.andExpect(jsonPath("$.username").value("zerobase"));
+	}
+}
---
+++ a_uction/src/test/java/com/example/a_uction/model/user/service/UserRegisterServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.a_uction.model.user.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.example.a_uction.model.user.dto.RegisterUser;
+import com.example.a_uction.model.user.dto.RegisterUser.Request;
+import com.example.a_uction.model.user.entity.UserEntity;
+import com.example.a_uction.model.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserRegisterServiceTest {
+
+	@Mock
+	private UserRepository userRepository;
+	@Mock
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@InjectMocks
+	private UserRegisterService userRegisterService;
+
+	@Test
+	@DisplayName("회원가입성공")
+	void register_SUCCESS() {
+		//given
+		given(userRepository.save(any()))
+			.willReturn(UserEntity.builder()
+				.userEmail("zerobase@gmail.com")
+				.username("zerobase")
+				.password("1234")
+				.phoneNumber("01012345678")
+				.build());
+		//when
+		RegisterUser registerUser =
+			userRegisterService.register(
+				new Request("ss@ss",
+					"1111",
+					"ss",
+					"01011111111")
+			);
+		ArgumentCaptor<UserEntity> captor = ArgumentCaptor.forClass(UserEntity.class);
+		//then
+		verify(userRepository, times(1)).save(captor.capture());
+		assertEquals("ss@ss", captor.getValue().getUserEmail());
+		assertEquals("zerobase@gmail.com", registerUser.getUserEmail());
+		assertEquals("zerobase", registerUser.getUsername());
+	}
+	//TODO CustomException 생성 후 회원가입 실패 테스트 작성
+}
