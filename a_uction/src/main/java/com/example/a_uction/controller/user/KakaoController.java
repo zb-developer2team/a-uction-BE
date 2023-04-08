3238829255ddbdc63ed8346bf26e9664513cafa5
sunsik <sunsig21c@gmail.com>
2023-04-08 22:46:19 +0900

    feat : [feat/#14/login-1] 서비스 자체 login controllerTestCode
    controller 테스트는 완료 하였으나
    service 테스트 코드는 완성하지 못했습니다.
    
    추가로 provider 에서 token validate 여러개를 추가 하였고
    userEntity 에 컬럼들을 추가했습니다.

--- a_uction/src/main/java/com/example/a_uction/controller/user/KakaoController.java
+++ a_uction/src/main/java/com/example/a_uction/controller/user/KakaoController.java
@@ -1,28 +1,28 @@
-package com.example.a_uction.controller.user;
-
-import com.example.a_uction.service.user.KakaoService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-@RequestMapping("/oauth")
-@RequiredArgsConstructor
-public class KakaoController {
-
-	private final KakaoService kakaoService;
-
-	@GetMapping("/kakao/callback")
-	public ResponseEntity<?> kakaoCallback(@RequestParam(name = "code") String code) {
-		log.info("[CODE = {}]", code);
-
-		String accessToken = kakaoService.getAccessToken(code);
-		return ResponseEntity.ok(accessToken);
-		// 현재 테스트 위해서 토큰 return , 추후에 리턴 값은 변경 할 것임
-	}
-}
+//package com.example.a_uction.controller.user;
+//
+//import com.example.a_uction.service.user.KakaoService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/oauth")
+//@RequiredArgsConstructor
+//public class KakaoController {
+//
+//	private final KakaoService kakaoService;
+//
+//	@GetMapping("/kakao/callback")
+//	public ResponseEntity<?> kakaoCallback(@RequestParam(name = "code") String code) {
+//		log.info("[CODE = {}]", code);
+//
+//		String accessToken = kakaoService.getAccessToken(code);
+//		return ResponseEntity.ok(accessToken);
+//		// 현재 테스트 위해서 토큰 return , 추후에 리턴 값은 변경 할 것임
+//	}
+//}
