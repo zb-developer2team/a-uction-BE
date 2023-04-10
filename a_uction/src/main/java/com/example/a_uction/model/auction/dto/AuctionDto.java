d413d82503c376e52c0b1e0a86287c165c5ab9e1
Eunseo-415 <eumseo@icloud.com>
2023-04-10 15:59:41 +0700

    feat/#51/delete_auction/0
    - 옥션 생성시 필수항목체크 등에 대한 리팩토링을 진행했습니다.

--- a_uction/src/main/java/com/example/a_uction/model/auction/dto/AuctionDto.java
+++ a_uction/src/main/java/com/example/a_uction/model/auction/dto/AuctionDto.java
@@ -7,6 +7,10 @@ import com.example.a_uction.model.auction.constants.TransactionStatus;
 import com.example.a_uction.model.auction.entity.AuctionEntity;
 import lombok.*;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 
@@ -18,13 +22,20 @@ public class AuctionDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Request {
+        @NotNull(message = "상품 이름을 입력하세요")
         private String itemName;
+        @NotNull(message = "상품의 상태를 입력하세요")
         private ItemStatus itemStatus;
+        @Min(value = 100, message = "최소 시작 금액은 100원 이상입니다.")
         private int startingPrice;
+        @Min(value = 100, message = "최소 호가 단위는 100원 이상입니다.")
         private int minimumBid;
-//        private Category category;
+        @Enumerated(EnumType.STRING)
+        private Category category;
         private AuctionStatus auctionStatus;
+        @NotNull(message = "경매 시작 시간을 입력하세요")
         private LocalDateTime startDateTime;
+        @NotNull(message = "경매 종료 시간을 입력하세요")
         private LocalDateTime endDateTime;
 
         public AuctionEntity toEntity(String userEmail){
